e0c6de3ff6bcd419899852367e8762e653125658
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-10 11:19:06 +0300

    data imported through context and passed to VideoList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { Context } from "../appContext/context";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import VideoList from "@/components/VideoList";
+import { useContext } from "react";
 
-export default function Home() {
+export default function HomePage() {
+  let { data } = useContext(Context);
+
   return (
     <div className="flex">
       <div className="flex-shrink-0 w-64">
@@ -9,6 +16,10 @@ export default function Home() {
       </div>
       <div className="flex-1">
         <Header />
+        <div className="container mx-auto p-4">
+          <h1 className="text-2xl font-bold mb-4">Home</h1>
+          <VideoList videos={data} />
+        </div>
       </div>
     </div>
   );
