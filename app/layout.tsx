1f8860ead0a8d855fb2a220be98ff58c2bb3b374
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-10 11:18:07 +0300

    minor debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import AppContext from "../appContext/context";
+import { AppContext } from "../appContext/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
