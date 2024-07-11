8ca3f913f2f4d9fb793b133f0689517e766a4153
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-11 12:41:47 +0300

    style: card styling updated

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../app/globals.css";
 
 interface VideoCardProps {
   video: {
@@ -11,7 +12,13 @@ interface VideoCardProps {
 
 const VideoCard: React.FC<VideoCardProps> = ({ video }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div
+      className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col card-background"
+      style={{
+        boxShadow:
+          "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+      }}
+    >
       <a
         href={`https://www.youtube.com/watch?v=${video.videoId}`}
         target="_blank"
@@ -19,9 +26,15 @@ const VideoCard: React.FC<VideoCardProps> = ({ video }) => {
       >
         <img className="w-full" src={video.thumbnail} alt={video.title} />
       </a>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{video.title}</div>
-        <p className="text-gray-700 text-base">{video.channelTitle}</p>
+      <div className="px-6 py-4 flex flex-col flex-grow justify-between">
+        <div>
+          <div className="font-bold text-xl mb-2">
+            {video.title.length > 50
+              ? video.title.substring(0, 30) + "..."
+              : video.title}
+          </div>
+        </div>
+        <p className="text-gray-700 text-base ">{video.channelTitle}</p>
       </div>
     </div>
   );
