95d32947b96e7b85e73bb8857224f60527384c0e
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-11 12:39:41 +0300

    feat: useSearchParams introduced to get dynamic query params

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,24 @@ import { Context } from "../appContext/context";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import VideoList from "@/components/VideoList";
-import { useContext } from "react";
+import { useSearchParams } from "next/navigation";
+import { useContext, useEffect } from "react";
 
 export default function HomePage() {
-  let { data } = useContext(Context);
+  let { data, setQuery } = useContext(Context);
+  const params = useSearchParams();
 
+  const category = params.get("category");
+
+  useEffect(() => {
+    setQuery(category);
+  }, [category, setQuery]);
+
+  const pageTitle = category
+    ? category.charAt(0).toUpperCase() + category.slice(1)
+    : "Home";
+
+  console.log(category);
   return (
     <div className="flex">
       <div className="flex-shrink-0 w-64">
@@ -17,7 +30,7 @@ export default function HomePage() {
       <div className="flex-1">
         <Header />
         <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-4">Home</h1>
+          <h1 className="text-2xl font-bold mb-4">{pageTitle}</h1>
           <VideoList videos={data} />
         </div>
       </div>
